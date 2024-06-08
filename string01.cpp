cdd0f06503538df166d954e00e5345a2aaa86bfa
Shuvo Chandro Paul <148526655+ShuvoDecodes@users.noreply.github.com>
2024-06-08 16:34:03 +0600

    Basic string

---
+++ string01.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+    // string str;
+    // getline(cin,str);
+    // cout<<str;
+
+    //?string concatation
+    // string firstName = "John ";
+    // string lastName = "Doe";
+    // string fullName = firstName.append(lastName);
+    // cout << fullName;
+
+    //?Some useful string function
+    // string firstName = "John Doe";
+    // cout << firstName.at(2);
+    // cout << firstName.length();
+    // cout << firstName.empty();
+    // cout << firstName.substr(1, 2);
+    // cout << firstName.at(2);
+    // cout << firstName.find("n");
+    // cout << firstName.replace(5, 3, "Cook");
+    // cout << firstName.insert(5, "Cook ");
+    // cout << firstName.erase(0, 5);
+    // cout << firstName.compare("John Do");
+
+    //? Use of push back and pop back function
+    // string name = "Scaler";
+    // cout << "Name before using push_back: " << name << endl;
+    // /// Let's add the word "Topics" at the end of the above string
+    // name.push_back(' ');
+    // name.push_back('T');
+    // name.push_back('o');
+    // name.push_back('p');
+    // name.push_back('i');
+    // name.push_back('c');
+    // name.push_back('s');
+    // cout << "Name after using push_back: " << name << endl;
+
+    // string name = "Scaler";
+    // cout << "Name before using pop_back: " << name << endl;
+    // /// using the pop_back() function
+    // name.pop_back();
+    // cout << "Name after using pop_back: " << name << endl;
+
+    return 0;
+}
